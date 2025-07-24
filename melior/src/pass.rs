851fdd265d97e17ea11656886c7110db4375a132
Austin Adams <git@austinjadams.com>
2025-07-23 17:38:49 -0700

    Make pass printing options visible

--- melior/src/pass.rs
+++ melior/src/pass.rs
@@ -8,7 +8,7 @@ pub mod transform;
 
 pub use self::{
     external::{create_external, ExternalPass, RunExternalPass},
-    manager::PassManager,
+    manager::{PassManager, PassIrPrintingOptions},
     operation_manager::OperationPassManager,
 };
 use qwerty_mlir_sys::MlirPass;
